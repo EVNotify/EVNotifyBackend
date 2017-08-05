16dff29ae8d488f66851f142ccd52b3480e1c35d
GPlay97 <GPlay97@users.noreply.github.com>
2017-08-06 01:46:45 +0200

    fix https issues; add handling for cors requests - add missing chain / correct it for https connections - fix handling of wrong requests to respond correctly to option calls or cors requests to prevent non establishing of https connection

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express'),
     app = express(),
     srv_config = require('./srv_config.json'),
     fs = require('fs'),
+    cors = require('cors'),
     https = require('https'),
-    httpsServer = https.createServer({key: fs.readFileSync(srv_config.PRIVATE_KEY_PATH, 'utf8'), cert : fs.readFileSync(srv_config.CERTIFICATE_PATH, 'utf8')}, app),
+    httpsServer = https.createServer({
+        ca: fs.readFileSync(srv_config.CHAIN_PATH, 'utf8'),
+        key: fs.readFileSync(srv_config.PRIVATE_KEY_PATH, 'utf8'),
+        cert : fs.readFileSync(srv_config.CERTIFICATE_PATH, 'utf8')}, app),
     bodyParser = require('body-parser'),
     user = require('./user'),
     telegram = require('./notification/telegram/'),
@@ -14,6 +18,8 @@ app.use(bodyParser.json());         // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+// required for cross origin resource sharing (CORS)
+app.use(cors(null, {credentials: true}));
 
 // different routes for the specific functions
 app.post('/login', user.login);                 // function to login an account
@@ -30,7 +36,11 @@ app.post('/syncsoc', user.syncSoC);             // function to sync the soc only
 app.use(function(req, res) {
     res.contentType('application/json');
 	res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(404).json({message: 'Unknown operation. Unable to handle request', error: 404});
+    /**
+     * we need to send http state 200 back to client - otherwise cors request calls (or requests such as an option call)
+     * will be declined and connection will not be established correctly
+     */
+    res.status(200).json({message: 'Unknown operation. Unable to handle request', error: 404});
 });
 
 // start telegram bot
