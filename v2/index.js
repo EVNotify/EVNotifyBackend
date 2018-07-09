fc7f2f7ca9327d869c84c9a3bb99d64802901fcc
GPlay97 <GPlay97@users.noreply.github.com>
2018-07-09 20:48:32 +0200

    preperation and first server initialization for v2

---
+++ v2/index.js
@@ -0,0 +1,97 @@
+/**
+ * @file index.js
+ * @author GPlay97
+ * @description Server initialization script
+ */
+const express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    srv_config = require('./srv_config.json'),
+    https = require('https'),
+    httpsServer = ((!srv_config.DEBUG && srv_config.CHAIN_PATH &&
+        srv_config.PRIVATE_KEY_PATH && srv_config.CERTIFICATE_PATH) ? https.createServer({
+        ca: fs.readFileSync(srv_config.CHAIN_PATH, 'utf-8'),
+        key: fs.readFileSync(srv_config.PRIVATE_KEY_PATH, 'utf-8'),
+        cert: fs.readFileSync(srv_config.CERTIFICATE_PATH, 'utf-8')
+    }) : false),
+    Rollbar = require('rollbar'),
+    rollbar = ((srv_config.ROLLBAR_TOKEN) ? new Rollbar({
+        accessToken: srv_config.ROLLBAR_TOKEN,
+        captureUncaught: true,
+        environment: ((srv_config.DEBUG) ? 'development' : 'production'),
+        captureUnhandledRejections: true
+    }) : false),
+    moesifExpress = require('moesif-express'),
+    moesif = ((srv_config.MOESIF_TOKEN) ? moesifExpress({
+        applicationId: srv_config.MOESIF_TOKEN,
+        identifyUser: (req, res) => req.body.akey
+    }) : false);
+
+// ensure that session secret is valid
+if (!srv_config.SESSION_SECRET || typeof srv_config.SESSION_SECRET !== 'string') throw new Error('No session secret given within config');
+// session handling
+app.use(session({
+    secret: srv_config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: (httpsServer !== false)
+    }
+}));
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Cross-Origin-Resource-Sharing support
+app.use(cors({
+    credentials: true,
+    /**
+     * Handles the origin for CORS request
+     * @param {String} origin the origin
+     * @param {Function} callback callback function
+     */
+    origin: (origin, callback) => {
+        if(!origin || origin === 'null') origin = '*';
+        callback(null, origin);
+    }
+}));
+
+app.use((req, res, next) => {
+    // attach akey to be tracked by rollbar
+    req.rollbar_person = {
+        id: ((req.body.akey && typeof req.body.akey.toString === 'function') ? req.body.akey.toString() : null)
+    };
+    next();
+});
+
+// moesif middleware
+if(moesif) app.use(moesif);
+
+// TODO last activity track - require new db module
+
+// TODO apply default headers
+
+// TODO routes
+
+// TODO route not found
+
+// TODO error handler
+
+// rollbar middleware
+if(rollbar) app.use(rollbar.errorHandler());
+
+// TODO telegram..
+
+// ensure that port is valid
+if(typeof srv_config.PORT !== 'number' || srv_config.PORT < 1024 || srv_config.PORT > 49151) throw new Error('Port must be between 1024 and 49151');
+
+// initialize the server
+if(srv_config.DEBUG) app.listen(srv_config.PORT, () => console.log('[HTTP] Server started on port ', srv_config.PORT));
+else if(httpsServer) httpsServer.listen(srv_config.PORT, () => console.log('[HTTPS] Server started on port ', srv_config.PORT));
+else throw new Error('Server could not be started');
