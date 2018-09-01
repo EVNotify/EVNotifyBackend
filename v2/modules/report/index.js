2c41167023ba389037d59987b3e98ba849cb57dd
GPlay97 <GPlay97@users.noreply.github.com>
2018-09-01 20:56:55 +0200

    bugfix and extend excel report for location data

--- v2/modules/report/index.js
+++ v2/modules/report/index.js
@@ -27,30 +27,58 @@ const createReport = (akey, token, callback) => {
                     const lng = settingsRes[0].lng || 'en';
 
                     // retrieve all data from statistics (soc)
-                    db.query('SELECT type, value, timestamp FROM statistics WHERE akey=? AND (type=? OR type=?) ORDER BY timestamp ASC', [
-                        akey, 'soc_display', 'soc_bms'
+                    db.query('SELECT type, value, timestamp FROM statistics WHERE akey=? AND (type=? OR type=? OR type=? OR type=? OR type=?) ORDER BY timestamp ASC', [
+                        akey, 'soc_display', 'soc_bms', 'latitude', 'longitude', 'gps_speed'
                     ], (err, dataRes) => {
                         if (!err && Array.isArray(dataRes)) {
                             let wb = new excel.Workbook(),
                                 ws = wb.addWorksheet(translation.translate('SOC_HISTORY', lng, true)),
+                                ws2 = wb.addWorksheet(translation.translate('LOCATION_HISTORY', lng, true)),
                                 curRow = 2,
                                 socDisplay = dataRes.filter(data => data.type === 'soc_display'),
-                                socBMS = dataRes.filter(data => data.type === 'soc_bms');
+                                socBMS = dataRes.filter(data => data.type === 'soc_bms'),
+                                socData = [...new Set(socDisplay.concat(socBMS).map(data => data.timestamp))], // unique timestamps
+                                latitude = dataRes.filter(data => data.type === 'latitude'),
+                                longitude = dataRes.filter(data => data.type === 'longitude'),
+                                gpsSpeed = dataRes.filter(data => data.type === 'gps_speed'),
+                                locationData = [...new Set(latitude.concat(longitude).concat(gpsSpeed).map(data => data.timestamp))];
 
-                            // build the header
+                            // build the header for the soc history
                             ['SOC_DISPLAY', 'SOC_BMS', 'DATE_TIME'].forEach((field, idX) => ws.cell(1, idX + 1).string(translation.translate(field, lng, true)));
-                            // build the rows
-                            dataRes.forEach(data => {
-                                let soc_display = (socBMS.filter(obj => obj.timestamp === data.timestamp)[0] || {}).value,
-                                    soc_bms = (socDisplay.filter(obj => obj.timestamp === data.timestamp)[0] || {}).value;
+                            // build the rows for the soc history
+                            socData.forEach(timestamp => {
+                                let soc_display = (socBMS.filter(obj => obj.timestamp === timestamp)[0] || {}).value,
+                                    soc_bms = (socDisplay.filter(obj => obj.timestamp === timestamp)[0] || {}).value;
 
                                 // SOC_DISPLAY
                                 if (soc_display != null) ws.cell(curRow, 1).number(parseFloat(soc_bms)|| 0);
                                 else ws.cell(curRow, 1).string('?');
-                                ws.cell(curRow, 2).date(new Date(data.timestamp * 1000)); // DATE_TIME
                                 // SOC_BMS
-                                if (soc_bms != null) ws.cell(curRow, 3).number(parseFloat(soc_display)|| 0);
-                                else ws.cell(curRow, 3).string('?');
+                                if (soc_bms != null) ws.cell(curRow, 2).number(parseFloat(soc_display)|| 0);
+                                else ws.cell(curRow, 2).string('?');
+                                // DATE_TIME
+                                ws.cell(curRow, 3).date(new Date(timestamp * 1000));
+                                curRow++; // increase the current row
+                            });
+                            curRow = 2; // reset the current row for next worksheet
+                            // build the header for the location history
+                            ['LATITUDE', 'LONGITUDE', 'SPEED', 'DATE_TIME'].forEach((field, idX) => ws2.cell(1, idX + 1).string(translation.translate(field, lng, true)));
+                            locationData.forEach(timestamp => {
+                                let lat = (latitude.filter(obj => obj.timestamp === timestamp)[0] || {}).value,
+                                    long = (longitude.filter(obj => obj.timestamp === timestamp)[0] || {}).value,
+                                    speed = (gpsSpeed.filter(obj => obj.timestamp === timestamp)[0] || {}).value;
+
+                                // LATITUDE
+                                if (lat != null) ws2.cell(curRow, 1).number(parseFloat(lat)|| 0);
+                                else ws2.cell(curRow, 1).string('?');
+                                // LONGITUDE
+                                if (long != null) ws2.cell(curRow, 2).number(parseFloat(long)|| 0);
+                                else ws2.cell(curRow, 2).string('?');
+                                // SPEED
+                                if (speed != null) ws2.cell(curRow, 3).number(parseFloat(speed)|| 0);
+                                else ws2.cell(curRow, 3).string('?');
+                                // DATE_TIME
+                                ws2.cell(curRow, 4).date(new Date(timestamp * 1000)); // DATE_TIME
                                 curRow++; // increase the current row
                             });
                             // TODO dynamic filename (tmp file which need to be unlinked and streamed to user..)
