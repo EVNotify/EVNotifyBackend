f9b677142ebd658ec1d4120d52dd4bb7d3d249f5
GPlay97 <GPlay97@users.noreply.github.com>
2018-10-01 22:42:39 +0200

    fix skip route for analytics

--- v2/index.js
+++ v2/index.js
@@ -30,7 +30,7 @@ const express = require('express'),
         applicationId: srv_config.MOESIF_TOKEN,
         identifyUser: req => req.body.akey,
         getApiVersion: () => '2',
-        skip: req => req.path === '/location' || req.path === '/debug' || req.path === '/soc' || req.path === 'extended'
+        skip: req => req.path === '/location' || req.path === '/debug' || req.path === '/soc' || req.path === '/extended'
     }) : false),
     db = require('./modules/db'),
     account = require('./modules/account'),
