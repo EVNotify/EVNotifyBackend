15f9960b9f71366fed6bf4a629347dbb7258da15
GPlay97 <GPlay97@users.noreply.github.com>
2019-08-12 19:15:26 +0200

    change to live environment

--- modules/integrations/paypal/index.js
+++ modules/integrations/paypal/index.js
@@ -4,7 +4,7 @@ const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
 const client = () => {
     if (!srv_config.PAYPAL_CLIENT_ID || !srv_config.PAYPAL_CLIENT_SECRET) return;
     return new checkoutNodeJssdk.core.PayPalHttpClient(
-        new checkoutNodeJssdk.core.SandboxEnvironment(srv_config.PAYPAL_CLIENT_ID, srv_config.PAYPAL_CLIENT_SECRET)
+        new checkoutNodeJssdk.core.LiveEnvironment(srv_config.PAYPAL_CLIENT_ID, srv_config.PAYPAL_CLIENT_SECRET)
     );
 };
 
