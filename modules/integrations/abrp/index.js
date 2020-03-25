381fcc665217eba0a4bf94f93f98b4fb70a92ed5
GPlay97 <GPlay97@users.noreply.github.com>
2020-03-25 15:06:46 +0100

    fix to use new oauth endpoint

--- modules/integrations/abrp/index.js
+++ modules/integrations/abrp/index.js
@@ -36,7 +36,7 @@ const auth = (req, res, next) => {
 };
 
 const getToken = (code, callback) => {
-    request.get(`${srv_config.ABRP_API_URL}/token?client_id=${srv_config.ABRP_CLIENT_ID}&client_secret=${srv_config.ABRP_CLIENT_SECRET}&code=${code}`, {
+    request.get(`${srv_config.ABRP_URL}/oauth/token?client_id=${srv_config.ABRP_CLIENT_ID}&client_secret=${srv_config.ABRP_CLIENT_SECRET}&code=${code}`, {
         json: true
     }, (err, resp, body) => {
         if (!err && body && body.access_token) {
